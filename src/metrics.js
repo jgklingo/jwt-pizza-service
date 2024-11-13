42539f8b0056f249972c40c399f73dc123b83342
jgklingo <jgklingo@byu.edu>
2024-11-12 17:44:30 -0700

    add(os metrics)

--- src/metrics.js
+++ src/metrics.js
@@ -1,46 +1,42 @@
-const config = require('./config.json');
-
-// Need to implement requestTracker function //
+const config = require('./config.js');
+const os = require('os');
 
 class Metrics {
   constructor() {
-    this.totalRequests = 0;
-    this.postRequests = 0;
-    this.deleteRequests = 0;
-    this.getRequests = 0;
-
     // This will periodically sent metrics to Grafana
     const timer = setInterval(() => {
-      this.sendMetricToGrafana('request', 'all', 'total', this.totalRequests);
-      this.sendMetricToGrafana('request', 'post', 'total', this.postRequests);
-      this.sendMetricToGrafana('request', 'delete', 'total', this.deleteRequests);
-      this.sendMetricToGrafana('request', 'get', 'total', this.getRequests);
+      this.sendMetricToGrafana('cpu', {}, 'pct', this.getCpuUsagePercentage());
+      this.sendMetricToGrafana('memory', {}, 'pct', this.getMemoryUsagePercentage());
     }, 10000);
     timer.unref();
   }
 
-  incrementPostRequests() {
-    this.totalRequests++;
-    this.postRequests++;
+  requestTracker(req, res, next) {
+    pass;
+    next();
   }
 
-  incrementDeleteRequests() {
-    this.totalRequests++;
-    this.deleteRequests++;
+  getCpuUsagePercentage() {
+    const cpuUsage = os.loadavg()[0] / os.cpus().length;
+    return cpuUsage.toFixed(2) * 100;
+  }
+  
+  getMemoryUsagePercentage() {
+    const totalMemory = os.totalmem();
+    const freeMemory = os.freemem();
+    const usedMemory = totalMemory - freeMemory;
+    const memoryUsage = (usedMemory / totalMemory) * 100;
+    return memoryUsage.toFixed(2);
   }
 
-  incrementGetRequests() {
-    this.totalRequests++;
-    this.getRequests++;
-  }
+  sendMetricToGrafana(metricPrefix, tags, metricName, metricValue) {
+    const tagsString = Object.keys(tags).map((key) => `,${key}=${tags[key]}`).join('');
+    const metric = `${metricPrefix},source=${config.metrics.source}${tagsString} ${metricName}=${metricValue}`;
 
-  sendMetricToGrafana(metricPrefix, httpMethod, metricName, metricValue) {
-    const metric = `${metricPrefix},source=${config.source},method=${httpMethod} ${metricName}=${metricValue}`;
-
-    fetch(`${config.url}`, {
+    fetch(`${config.metrics.url}`, {
       method: 'post',
       body: metric,
-      headers: { Authorization: `Bearer ${config.userId}:${config.apiKey}` },
+      headers: { Authorization: `Bearer ${config.metrics.userId}:${config.metrics.apiKey}` },
     })
       .then((response) => {
         if (!response.ok) {
