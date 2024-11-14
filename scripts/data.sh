666f83e414461510eb5fd85ac0c9520cb95810ce
jgklingo <jgklingo@byu.edu>
2024-11-14 11:31:16 -0700

    add(scripts)

---
+++ scripts/data.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Check if host is provided as a command line argument
+if [ -z "$1" ]; then
+  echo "Usage: $0 <host>"
+  echo "Example: $0 http://localhost:3000"
+  exit 1
+fi
+host=$1
+
+# Check if jq is installed
+if ! command -v jq &> /dev/null
+then
+    echo "jq could not be found, please install it to proceed."
+    exit 1
+fi
+
+# Login as admin
+response=$(curl -s -f -X PUT $host/api/auth -d '{"email":"a@jwt.com", "password":"admin"}' -H 'Content-Type: application/json')
+if [ $? -ne 0 ]; then
+    echo "Failed to login as admin"
+    echo $response
+    exit 1
+fi
+token=$(echo $response | jq -r '.token')
+echo $response
+
+# Add users
+curl -s -f -X POST $host/api/auth -d '{"name":"pizza diner", "email":"d@jwt.com", "password":"diner"}' -H 'Content-Type: application/json'
+curl -s -f -X POST $host/api/auth -d '{"name":"pizza franchisee", "email":"f@jwt.com", "password":"franchisee"}' -H 'Content-Type: application/json'
+
+# Add menu
+curl -s -f -X PUT $host/api/order/menu -H 'Content-Type: application/json' -d '{ "title":"Veggie", "description": "A garden of delight", "image":"pizza1.png", "price": 0.0038 }'  -H "Authorization: Bearer $token"
+curl -s -f -X PUT $host/api/order/menu -H 'Content-Type: application/json' -d '{ "title":"Pepperoni", "description": "Spicy treat", "image":"pizza2.png", "price": 0.0042 }'  -H "Authorization: Bearer $token"
+curl -s -f -X PUT $host/api/order/menu -H 'Content-Type: application/json' -d '{ "title":"Margarita", "description": "Essential classic", "image":"pizza3.png", "price": 0.0042 }'  -H "Authorization: Bearer $token"
+curl -s -f -X PUT $host/api/order/menu -H 'Content-Type: application/json' -d '{ "title":"Crusty", "description": "A dry mouthed favorite", "image":"pizza4.png", "price": 0.0028 }'  -H "Authorization: Bearer $token"
+curl -s -f -X PUT $host/api/order/menu -H 'Content-Type: application/json' -d '{ "title":"Charred Leopard", "description": "For those with a darker side", "image":"pizza5.png", "price": 0.0099 }'  -H "Authorization: Bearer $token"
+
+# Add franchise and store
+curl -s -f -X POST $host/api/franchise -H 'Content-Type: application/json' -d '{"name": "pizzaPocket", "admins": [{"email": "f@jwt.com"}]}'  -H "Authorization: Bearer $token"
+curl -s -f -X POST $host/api/franchise/1/store -H 'Content-Type: application/json' -d '{"franchiseId": 1, "name":"SLC"}'  -H "Authorization: Bearer $token"
